b3e13affc878c3ad25d58002af8a19a05326f8ce
Seinu <Seinuve@gmail.com>
2023-08-28 13:02:53 -0600

    init and completion of task 1

---
+++ Assignment 1/src/BilyeuKadenAssignment1.java
@@ -0,0 +1,24 @@
+/*
+ * Kaden Bilyeu
+ * CS 1450-002 (Tuesday/Thursday)
+ * Due Thursday, August 31st 1:40pm
+ * Assignment 1
+ * To get back into the flow of writing code, by reviewing the basics of Java. Such as loops, arrays, and files
+ */
+
+ import java.util.*;
+
+public class BilyeuKadenAssignment1
+{
+    public static void main(String[] args) 
+    {
+
+        int[] array = {17, 10, 2, 16, 8, 15, 9, 17, 14, 18, 1, 19, 17, 2, 0};
+
+        Arrays.sort(array);
+
+        System.out.println("The sorted array is: " + Arrays.toString(array));
+        
+    }
+
+}
