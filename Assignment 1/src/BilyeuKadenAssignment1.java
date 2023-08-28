4470520cb2c25a23be7d531c3863576cc0d8626e
Seinu <Seinuve@gmail.com>
2023-08-28 14:34:07 -0600

    task 4 complete

--- Assignment 1/src/BilyeuKadenAssignment1.java
+++ Assignment 1/src/BilyeuKadenAssignment1.java
@@ -65,9 +65,9 @@ public class BilyeuKadenAssignment1
 
     public static void read_matrix_from_file(int[][] matrix, Scanner inputFile)
     {
-        for(int i = 0; i < matrix.length; i++)
+        for(int i = matrix.length- 1; i >= 0; i--)
         {
-            for(int ii = 0; ii < matrix[i].length; ii++)
+            for(int ii = matrix[i].length - 1; ii >= 0; ii--)
             {
                 matrix[i][ii] = inputFile.nextInt();
             }
