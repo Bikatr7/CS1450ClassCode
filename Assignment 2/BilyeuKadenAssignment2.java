0a9506bfaeda1d45d84a2e8ccc3e500037aef76b
Seinu <Seinuve@gmail.com>
2023-09-05 11:45:38 -0600

    got to the display sharks part but ran into an error

--- Assignment 2/BilyeuKadenAssignment2.java
+++ Assignment 2/BilyeuKadenAssignment2.java
@@ -12,11 +12,69 @@
  // Sorry, been writing python too long so I messed up my func names.
  // And as for clearConsole, that's for my own benefit.
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+ 
+
 public class BilyeuKadenAssignment2
 {
 
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args) throws IOException
     {
+        File fileName = new File("sharks.txt");
+        Scanner inputFile = new Scanner(fileName);
+
+        String type;
+        int age;
+        String name;
+
+        int numOfSharks = inputFile.nextInt();
+
+        Shark[] sharks = new Shark[numOfSharks];
+
+        for(int i = 0; i < numOfSharks; i++)
+        {
+
+            type = inputFile.next();
+            age = inputFile.nextInt();
+            name = inputFile.nextLine();
+
+            switch(type)
+            {
+                case "GreatWhite":
+                    sharks[i] = new GreatWhite(age, name);
+                    break;
+                case "Hammerhead":
+                    sharks[i] = new Hammerhead(age, name);
+                    break;
+                case "Tiger":
+                    sharks[i] = new Tiger(age, name);
+                    break;
+                case "Zebra":
+                    sharks[i] = new Zebra(age, name);
+                    break;
+            }
+
+        }
+
+        inputFile.close();
+
+        displaySharkTable(sharks);
+
+
+    }
+
+    public static void displaySharkTable(Shark[] sharks)
+    {
+        System.out.println("-------------------------------------------------------------------------------------");
+		System.out.printf("%-10s%-10s%-10s%-7s%n", "Type", "Age", "Name", "Physical Description");
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        for (Shark shark : sharks)
+        {
+            System.out.printf("%-10s%-10s%-10s%-7s%n", shark.getType(), shark.getAge(), shark.getName(), shark.physicalDescription());
+        }
 
     }
 
