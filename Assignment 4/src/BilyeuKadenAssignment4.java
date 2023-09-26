622e322ea70f8cb10304e6bcb8d826c742e22163
Bikatr7 <Tetralon07@gmail.com>
2023-09-26 15:20:15 -0600

    added player and team classes

--- Assignment 4/src/BilyeuKadenAssignment4.java
+++ Assignment 4/src/BilyeuKadenAssignment4.java
@@ -20,3 +20,99 @@ public class BilyeuKadenAssignment4
     }
 
 }
+
+
+class Team 
+{
+
+    private String name;
+    private int numberSpots;
+    private Player [] roster;
+
+
+    public Team (String name, int numberSpots)
+    {
+        this.name = name;
+        this.numberSpots = numberSpots;
+        this.roster = new Player[numberSpots];
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getNumberSpots()
+    {
+        return numberSpots;
+    }
+
+
+    public void addPlayer(int spot, Player player)
+    {
+        roster[spot] = player;
+    }
+
+    public Player getPlayer(int spot)
+    {
+        return roster[spot];
+    }
+
+    public void displayTeam()
+    {
+        return;
+    }
+}
+
+class Player implements Comparable <Player>
+{
+
+    private String team;
+    private int ranking;
+    private String name;
+
+    public Player (String team, int ranking, String name)
+    {
+        this.team = team;
+        this.ranking = ranking;
+        this.name = name;
+    }
+
+    public String getTeam()
+    {
+        return team;
+    }
+
+    public int getRanking()
+    {
+        return ranking;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%s\t%d\t\t%-10s",team, ranking, name);
+    }
+
+    @Override
+    public int compareTo(Player otherPlayer)
+    {
+        if (this.ranking < otherPlayer.ranking)
+        {
+            return -1;
+        }
+        else if (this.ranking > otherPlayer.ranking)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
