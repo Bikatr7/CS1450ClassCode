69dae5a3132d49a063817fb71b34d16eacd69d85
Bikatr7 <Tetralon07@gmail.com>
2023-09-26 13:52:52 -0600

    base laid

---
+++ Assignment 4/src/BilyeuKadenAssignment4.java
@@ -0,0 +1,22 @@
+/*
+ * Kaden Bilyeu
+ * CS 1450-002 (Tuesday/Thursday)
+ * Due Thursday, September 28 1:40pm
+ * Assignment 4
+ * This assignment allows us to get familiar with Java's comparable interface by using it to interact with a team and its roster.
+ * Read a team from a file, display them, and rank them.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+ 
+public class BilyeuKadenAssignment4
+{
+
+    public static void main(String[] args) throws IOException
+    {
+
+    }
+
+}
