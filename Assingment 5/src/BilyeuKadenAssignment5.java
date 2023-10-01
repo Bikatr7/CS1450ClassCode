e8414bc1eb15b092c7e470d9ba9c0a79499dfe25
Bikatr7 <Tetralon07@gmail.com>
2023-10-01 17:32:48 -0600

    added generic stack and part 1 base

--- Assingment 5/src/BilyeuKadenAssignment5.java
+++ Assingment 5/src/BilyeuKadenAssignment5.java
@@ -7,12 +7,106 @@
  * The goal of the assignment is to implement and manipulate generic stacks in Java, focusing on adding zeros after even numbers, sorting, and rearranging elements based on a given value.
  */
 
+ import java.util.*;
+
  public class BilyeuKadenAssignment5
  {
 
     public static void main(String[] args)
     {
-        
+
+        int[] values = {10, 1, 42, 15, 62, 8, 17, 2};
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int value : values)
+        {
+            stack.push(value);
+        }
+
     }
 
- }
+//-------------------start-of-addZeroAfterEvenValues()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static void addZeroAfterEvenValues (Stack<Integer> stack)
+    {
+        Stack<Integer> tempStack = new Stack<Integer>();
+
+        while(!stack.isEmpty())
+        {
+            int value = stack.pop();
+
+            if(value % 2 == 0)
+            {
+                tempStack.push(value);
+                tempStack.push(0);
+            }
+            else
+            {
+                tempStack.push(value);
+            }
+        }
+
+    }
+
+//-------------------start-of-sortStack()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static void printStack (Stack<Integer> stack)
+    {
+
+    }
+
+ } // BilyeuKadenAssignment5
+
+ //-------------------start-of-GenericStack---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ class GenericStack<E>
+ {
+    private ArrayList<E> list;
+
+//-------------------start-of-GenericStack()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public GenericStack()
+    {
+        list = new ArrayList<E>();
+    }
+
+//-------------------start-of-isEmpty()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public boolean isEmpty()
+    {
+        return list.isEmpty();
+    }
+
+//-------------------start-of-getSize()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public int getSize()
+    {
+        return list.size();
+    }
+
+//-------------------start-of-peek()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public E peek()
+    {
+        return list.get(getSize() - 1);
+    }
+
+//-------------------start-of-pop()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public E pop()
+    {
+        E o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+
+//-------------------start-of-push()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public E push(E o)
+    {
+        list.add(o);
+        return o;
+    }
+
+ } // GenericStack
