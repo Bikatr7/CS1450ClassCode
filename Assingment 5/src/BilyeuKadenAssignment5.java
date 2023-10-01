53acb495098cd14325eef5612bd6f9a657468c4a
Bikatr7 <Tetralon07@gmail.com>
2023-10-01 16:42:27 -0600

    added assignment 5 base

---
+++ Assingment 5/src/BilyeuKadenAssignment5.java
@@ -0,0 +1,18 @@
+/*
+ * Kaden Bilyeu
+ * CS 1450-002 (Tuesday/Thursday)
+ * Due Thursday, October 5th 1:40pm
+ * Assignment 5
+ * This assignment allows us to get familiar with the idea of generics and stacks, as well as how to use them.
+ * The goal of the assignment is to implement and manipulate generic stacks in Java, focusing on adding zeros after even numbers, sorting, and rearranging elements based on a given value.
+ */
+
+ public class BilyeuKadenAssignment5
+ {
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+ }
