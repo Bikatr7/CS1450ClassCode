62da899eef9ff95ad36673f130368c6927cee3cd
Bikatr7 <Tetralon07@gmail.com>
2023-10-24 15:42:32 -0600

    added assignment 7 team file, as well as base main

---
+++ Assignment 7/src/BilyeuKadenAssignment7.java
@@ -0,0 +1,16 @@
+/*
+ * Kaden Bilyeu
+ * CS 1450-002 (Tuesday/Thursday)
+ * Due Thursday, October 26th 1:40pm
+ * Assignment 7
+ * This assignment allows us to get more familiar with queues and priority queues.
+ * The goal of the assignment is to manipulate a team and simulate a game using queues.
+ */
+
+public class BilyeuKadenAssignment7 
+{
+    public static void main(String[] args) 
+    {
+
+    }    
+}
