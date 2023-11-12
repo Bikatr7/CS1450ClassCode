43323bac8ed5b73cfb8c7f789fa611c628e6fd8a
Bikatr7 <Tetralon07@gmail.com>
2023-11-11 21:13:49 -0700

    adjusted docstring at front and start of word class

--- Assignment 9/src/BilyeuKadenAssignment9.java
+++ Assignment 9/src/BilyeuKadenAssignment9.java
@@ -3,15 +3,15 @@
  * CS 1450-002 (Tuesday/Thursday)
  * Due Tuesday, November 14th 1:40pm
  * Assignment 9
- * This assignment allows us to get more familiar with 2D arrays, ArrayLists, queues, and iterators.
- * The goal of the assignment is to decode a secret message from parrots using these tools
+ * *This assignment copypasta*
+ * *The goal of this copypasta*
  */
 
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
- public class BilyeuKadenAssignment9
+public class BilyeuKadenAssignment9
 {
 
     public static void main(String[] args) throws IOException
@@ -19,5 +19,13 @@ import java.util.*;
 
 
     }
+
+}
+
+class Word
+{
+    private String letters;
+    private Boolean abecedarian;
+
 
 }
