74c05df95d5f9b59d7d90fba06be95c420114359
Bikatr7 <Tetralon07@gmail.com>
2023-11-12 00:53:27 -0700

    some

--- Assignment 9/src/BilyeuKadenAssignment9.java
+++ Assignment 9/src/BilyeuKadenAssignment9.java
@@ -22,10 +22,61 @@ public class BilyeuKadenAssignment9
 
 }
 
+//-------------------start-of-Word--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class Word
 {
     private String letters;
     private Boolean abecedarian;
 
+//-------------------start-of-Word()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-}
+    public Word(String letters)
+    {
+        this.letters = letters;
+        this.abecedarian = abecedarianTest();
+    }
+
+//-------------------start-of-isAbecedarian()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public boolean isAbecedarian()
+    {
+        return abecedarian;
+    }
+
+//-------------------start-of-abecedarianTest()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    private boolean abecedarianTest()
+    {
+        for(int i = 0; i < letters.length() - 1; i++)
+        {
+            if(letters.charAt(i) > letters.charAt(i + 1))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+//-------------------start-of-print()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public String print()
+    {
+        return String.format("%s\t\t%b", letters, abecedarian);
+    }
+
+//-------------------start-of-compareTo()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Compares two words based on the alphabetical order of the letters in the word.
+     * @param otherWord
+     * @return integer value (-1, 0, 1) based on result of comparing two words.
+     */
+
+    public int compareTo(Word otherWord)
+    {
+
+    }
+
+} // end of Word
